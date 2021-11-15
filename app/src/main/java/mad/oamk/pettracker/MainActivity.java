9268e3c740ea07e90d040a97b5686abaf2f3dc33
kffijr8082095f <martti.alasaarela@gmail.com>
2021-11-15 22:08:45 +0200

    firebase siging

--- app/src/main/java/mad/oamk/pettracker/MainActivity.java
+++ app/src/main/java/mad/oamk/pettracker/MainActivity.java
@@ -2,13 +2,30 @@ package mad.oamk.pettracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        // Initialize Firebase and check if the user is signed in
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user == null) {
+            // Not signed in, launch the Sign In activity
+            Intent intent = new Intent(this, SignInActivity.class);
+            startActivity(intent);
+        }
     }
 }
