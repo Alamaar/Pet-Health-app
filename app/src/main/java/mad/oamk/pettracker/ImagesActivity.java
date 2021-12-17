3a53a7fbf9398892b143112ec849182bd3330163
kffijr8082095f <martti.alasaarela@gmail.com>
2021-12-17 14:59:00 +0200

    images init

---
+++ app/src/main/java/mad/oamk/pettracker/ImagesActivity.java
@@ -0,0 +1,19 @@
+package mad.oamk.pettracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ImagesActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_images);
+
+
+
+
+
+    }
+}
